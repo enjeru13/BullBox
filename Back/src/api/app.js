49fd07242a77beafddefa2f28b19f368abbcd90d
enjeru13 <angeleduardocegarrataborda@gmail.com>
2024-11-11 17:11:01 -0400

    configuring server

--- Back/src/api/app.js
+++ Back/src/api/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = [
-  process.env.FRONTEND_URL || "https://bull-box-front.vercel.app/"
+  process.env.FRONTEND_URL || "http://localhost:5173/"
 ];
 
 app.use(
