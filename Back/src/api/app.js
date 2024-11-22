ddca30fc2584aba2bbf970bfaf140b720cf011dd
enjeru13 <angeleduardocegarrataborda@gmail.com>
2024-11-22 15:51:08 -0500

    add netlify front

--- Back/src/api/app.js
+++ Back/src/api/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = [
-  process.env.FRONTEND_URL || "https://bull-box.vercel.app"
+  process.env.FRONTEND_URL || "https://bull-box.netlify.app"
 ];
 console.log(process.env.FRONTEND_URL)
 
